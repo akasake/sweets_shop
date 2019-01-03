cd37fc1e8d112dc48b1948371842b49765859276
Akasake <pham_see@hotmail.com>
2019-01-03 04:19:13 +0100

    added library and ajax to make order

---
+++ assets/js/sweets_shop.js
@@ -0,0 +1,23 @@
+(function ($) {
+    $(document).ready(function () {
+        $('.sweets_shop a').on('click', function(e) {
+            e.preventDefault();
+            var sweet = $(this).data( "sweet" );
+            $.ajax({
+                type: "POST",
+                data: { 
+                    sweet: sweet
+                },
+                url: "/ajax/sweets_shop/make_order",
+                success: function(resp) {
+                    $('.'+sweet+'-order-list').after(`<p>${resp}</p>`);
+                    $('orders-list').remove();
+                    $('.'+sweet+'-order-btn').remove();
+                },
+                error: function() {
+                    console.log('error');
+                }
+            });
+        });
+    });
+}(jQuery));
---
+++ src/Controller/AjaxController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Drupal\thomas_more_social_media\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Database\Connection;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+
+class AjaxController extends ControllerBase{
+    public function counter(Request $request){
+
+        try{
+            $sweet = $request->get('sweet');
+            $result = \Drupal::database()->update('sweets_shop_'.$sweet.'_data')->fields([
+                'made' => 1,
+            ])->condition('made', 0)->execute();
+
+            
+        }catch(exception $e){
+            return new Response('There was a problem making your order');
+        }
+        return new Response('Your order has been made');
+
+        
+    }
+}
